ed458f188c91f4d8ca27fe00fa99029302ecac7e
rossgreer <rossgreer@yahoo.com>
2024-02-20 14:53:35 -0800

    Update HW2_1.py

--- HW2/HW2_1.py
+++ HW2/HW2_1.py
@@ -13,7 +13,7 @@ sigma = np.array([[50,0],[0,50]])
 mu = np.array([[60, 50], [10, 40]])
 
 def f(x, y):
-	return ((final_loc[0]-x)**2 + (final_loc[1]-y)**2)/20000 + 10000*(1/(2*np.pi*np.linalg.det(sigma)))*np.exp(-.5*(np.matmul(np.array([x-mu[0,0], y-mu[0,1]]),np.matmul(np.linalg.pinv(sigma), np.atleast_2d(np.array([x-mu[0,0], y-mu[0,1]])).T)))[0]) + 10000*(1/(2*np.pi*np.linalg.det(sigma)))*np.exp(-.5*(np.matmul(np.array([x-mu[1,0], y-mu[1,1]]),np.matmul(np.linalg.pinv(sigma), np.array([x-mu[1,0], y-mu[1,1]])))))
+	return ((final_loc[0]-x)**2 + (final_loc[1]-y)**2)/20000 + 10000*(1/(2*np.pi*np.linalg.det(sigma)))*np.exp(-.5*(np.matmul(np.array([x-mu[0,0], y-mu[0,1]], dtype=object),np.matmul(np.linalg.pinv(sigma), np.atleast_2d(np.array([x-mu[0,0], y-mu[0,1]], dtype=object)).T)))[0]) + 10000*(1/(2*np.pi*np.linalg.det(sigma)))*np.exp(-.5*(np.matmul(np.array([x-mu[1,0], y-mu[1,1]], dtype=object),np.matmul(np.linalg.pinv(sigma), np.array([x-mu[1,0], y-mu[1,1]], dtype=object)))))
 
 x = np.linspace(0, 100, 100)
 y = np.linspace(0, 100, 100)
